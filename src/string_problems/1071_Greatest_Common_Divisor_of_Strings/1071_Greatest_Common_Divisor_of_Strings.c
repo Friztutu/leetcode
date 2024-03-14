dff7080758159898cf25cc36998c3e32b8d3234d
Alex Erofeev <fr1mate@pop-os.localdomain>
2024-03-14 22:47:56 +0300

    (develop)'

--- src/string_problems/1071_Greatest_Common_Divisor_of_Strings/1071_Greatest_Common_Divisor_of_Strings.c
+++ src/string_problems/1071_Greatest_Common_Divisor_of_Strings/1071_Greatest_Common_Divisor_of_Strings.c
@@ -1,13 +1,9 @@
-#include "string.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 char* gcdOfStrings(char* str1, char* str2);
 
 int main(void) {
   return 0;
-}
-
-char* gcdOfStrings(char* str1, char* str2) {
-  char* resultString = malloc(sizeof(char) * strlen(str1));
-
-  
 }
