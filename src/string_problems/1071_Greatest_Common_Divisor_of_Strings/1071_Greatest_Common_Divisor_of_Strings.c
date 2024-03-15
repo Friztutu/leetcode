66ae75ab624d9e7a150431d311b3af8342c1b68c
Alex Erofeev <fr1mate@pop-os.localdomain>
2024-03-16 01:47:20 +0300

    (develop) 1071 Greatest Common Divisor of Strings

--- src/string_problems/1071_Greatest_Common_Divisor_of_Strings/1071_Greatest_Common_Divisor_of_Strings.c
+++ src/string_problems/1071_Greatest_Common_Divisor_of_Strings/1071_Greatest_Common_Divisor_of_Strings.c
@@ -1,9 +1,33 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char* gcdOfStrings(char* str1, char* str2);
 
 int main(void) {
+  char str1[] = "ABABAB";
+  char str2[] = "AB";
+  char* result = gcdOfStrings(str1, str2);
+  printf("result = %s\n", result);
   return 0;
+}
+
+char* gcdOfStrings(char* str1, char* str2) {
+  if (strlen(str2) > strlen(str1)) {
+    return gcdOfStrings(str2, str1);
+  } else if (strcmp(str1, str2) == 0) {
+    return str1;
+  }
+
+  size_t currOffset = 0;
+
+  while (*str1 == str2[currOffset]) {
+    str1 += 1, currOffset++;
+  }
+
+  if (currOffset == strlen(str2)) {
+    return gcdOfStrings(str1, str2);
+  }
+
+  return "";
 }
