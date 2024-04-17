e66a2dc80a31b339607dc2e1b429e0a4afd68f07
Alex Erofeev <fr1mate@pop-os.localdomain>
2024-04-17 22:26:13 +0300

    (develop)

--- src/11_Container_With_Most_Water/11_Container_With_Most_Water.c
+++ src/11_Container_With_Most_Water/11_Container_With_Most_Water.c
@@ -3,14 +3,35 @@
 int maxArea(int* height, int heightSize);
 
 int main(void) {
-  int height[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-  printf("%d\n", maxArea(height, 9));
+  int height1[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+  printf("%d\n", maxArea(height1, 9));
+
+  int height2[] = {1, 1};
+  printf("%d\n", maxArea(height2, 2));
   return 0;
 }
 
 int maxArea(int* height, int heightSize) {
-  int left = 0, previousLeft = 0;
-  int right = heightSize - 1, previousRight = heightSize - 1;
+  int left = 0, right = heightSize - 1;
+  int bottonSide = right - left;
+  int side = height[left] > height[right] ? height[right] : height[left];
+  int result = bottonSide * side;
 
-  
+  while (left < right) {
+    bottonSide = right - left;
+    side = height[left] > height[right] ? height[right] : height[left];
+    int square = side * bottonSide;
+
+    if (result < square) {
+      result = square;
+    }
+
+    if (height[left] > height[right]) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return result;
 }
